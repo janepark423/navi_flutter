b81e9e52413bd32f37e6306bd0f740d3c14ebb01
Jain <janepark423@gmail.com>
2022-11-28 22:18:48 +0900

    first commit

---
+++ lib/screen/route_one_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class RouteOneScreen extends StatelessWidget {
+  const RouteOneScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Route one'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(onPressed: (){
+              Navigator.of(context).pop;
+            }, child: Text('pop'),),
+          ],
+        ),
+      ),
+    );
+  }
+}
